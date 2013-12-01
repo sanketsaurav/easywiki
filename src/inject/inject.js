5ca6861bc596440e598a28f456ff7f4ba25485c2
Sanket Saurav <sanketsaurav@gmail.com>
2013-12-01 20:22:55 +0530

    Minor fixes in popover.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -11,27 +11,46 @@
 // 	}
 // 	}, 10);
 // });
-WIKI_URL_REGEX = /^(http|https):\/\/[a-z]+.wikipedia.org\/wiki\/[a-zA-Z0-9_()#%]+$/i
+WIKI_URL_REGEX = /^(http|https):\/\/[a-z]+.wikipedia.org\/wiki\/[a-zA-Z0-9_()#%,.]+$/i
 CLASS_MATCHER = /easy-wiki-popover/i
 
 $(document).ready(function() {
+	
 	$("a").on('mouseenter', function() {
 		if (this.href.match(WIKI_URL_REGEX)) {
 			if (!($(this).hasClass("easy-wiki-popover"))) {
 				$(this).addClass("easy-wiki-popover");
-				$(this).popover({placement: 'bottom'}).popover("show");
+
+				$(this).popover({
+					placement: 'auto',
+					html: true,
+					trigger: 'manual'
+				}).on("mouseenter", function() {
+					var _this = this;
+			        $(this).popover("show");
+			        $(this).siblings(".popover").on("mouseleave", function () {
+			            $(_this).popover('hide');
+			        });
+				}).on("mouseleave", function() {
+					var _this = this;
+			        setTimeout(function () {
+			            if (!$(".popover:hover").length) {
+			                $(_this).popover("hide")
+			            }
+			        }, 100);
+				}).popover("show");
 			}
 
 		}
 	});
 
-	$("a").on('mouseleave', function() {
-		if (this.href.match(WIKI_URL_REGEX)) {
-			if ($(this).hasClass("easy-wiki-popover")) {
-				$(this).popover("hide");
-			}
+	// $("easy-wiki-popover").on('mouseleave', function() {
+	// 	if (this.href.match(WIKI_URL_REGEX)) {
+	// 		if ($(this).hasClass("easy-wiki-popover")) {
+	// 			$(this).popover("hide");
+	// 		}
 
-		}
-	});
+	// 	}
+	// });
 
 });
