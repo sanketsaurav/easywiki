387d1518449a7a5ad7713ee6a04b1641d4e23331
avck <avck.tara@gmail.com>
2013-12-02 01:00:42 +0530

    OnOff Function

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,8 +6,25 @@
 
 
 //example of using a message handler from the inject scripts
+var _OnOff=1;
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
+chrome.browserAction.onClicked.addListener(function(){
+	if(_OnOff)
+	{
+		var obje={text:"on"};
+		chrome.browserAction.setBadgeText(obje);
+		
+	}
+	else{
+		var obje={text:"off"};
+		chrome.browserAction.setBadgeText(objec);
+
+	}
+		_OnOff+=1;
+		_OnOff%=2;
+	
+});
